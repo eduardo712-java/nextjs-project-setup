826f1643d1b824cb51f1b4abed153ede42d80d43
eduardo712-java <116765061+eduardo712-java@users.noreply.github.com>
2025-06-03 14:54:07 -0300

    Add .next/static/chunks/src_app_login_page_tsx_c26f6b24._.js 1748973245805

---
+++ .next/static/chunks/src_app_login_page_tsx_c26f6b24._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_c7df88a5._.js"
+],
+    source: "dynamic"
+});
